044a57bd801e03997cbd286eb7f3466ee5041291
DrDisagree <crazymahmud08@gmail.com>
2024-04-12 16:40:01 +0000

    addons: Import more IconShapes from Iconify
    Co-authored-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: ifloraxd <ifloraxd@gmail.com>

--- config.mk
+++ config.mk
@@ -242,11 +242,15 @@ PRODUCT_PACKAGES += \
     IconShapeFlowerOverlay \
     IconShapeHeartOverlay \
     IconShapeHexagonOverlay \
+    IconShapeIosOverlay \
     IconShapeLeafOverlay \
     IconShapeMeowOverlay \
     IconShapePebbleOverlay \
     IconShapeRoundedHexagonOverlay \
     IconShapeRoundedRectOverlay \
+    IconShapeSamsungOverlay \
+    IconShapeScrollOverlay \
+    IconShapeStretchedOverlay \
     IconShapeSquareOverlay \
     IconShapeSquircleOverlay \
     IconShapeStretchedOverlay \
