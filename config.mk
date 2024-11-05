f10a56a6c075e77cee28f57e645cad673d09c53f
Drenzzz <116653610+drenzzz@users.noreply.github.com>
2024-11-05 21:55:09 +0700

    overlays: Add brightness slider styles [3/3] (#3)
    Squashed in:
    
    Author: Pzqqt <821026875@qq.com>
    Date:   Sat Jun 10 14:12:28 2023 +0800
    
        addons: brightness_slider: Fixup
    
        - Replace all `harmful_*` properties that define corner radius with sensible values.
        - Remove useless resources.
    
        Memo:
          - `bg_qs_brightness_auto_on` and `bg_qs_brightness_auto_off`: The background of the auto-brightness button. (unused)
          - `brightness_mirror_background`: The background that appears when dragging the brightness bar.
          - `brightness_progress_drawable` and `brightness_progress_full_drawable`: The style of the brightness bar.
          - `ic_qs_brightness_auto_on` and `ic_qs_brightness_auto_off`: The icon of the auto-brightness button.
    
    Author: rdx420 <padraramesh420@gmail.com>
    Date:   Sat May 27 23:12:57 2023 +0200
    
        addons: Add more brightness slider styles
    
        Credit - https://github.com/Mahmud0808/Iconify
    
    Author: Pzqqt <821026875@qq.com>
    Date:   Mon Jun 5 13:49:27 2023 +0800
    
        themes: overlays: Some tweaks to BrightnessSliderGradient
    
        And rename "Gradient" to "GradientRoundedBar"
    
    Author: Pzqqt <821026875@qq.com>
    Date:   Sat Jun 3 13:39:45 2023 +0800
    
        themes: overlays: Add gradient brightness slider
    
        Taken from [Iconify](https://github.com/Mahmud0808/Iconify)
    
    Author: Pranav Vashi <neobuddy89@gmail.com>
    Date:   Sat Jun 3 01:05:37 2023 +0530
    
        addons: Add category for brightness slider themes
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Author: Ghosuto <clash.raja10@gmail.com>
    Date:   Sat May 6 05:53:15 2023 +0000
    
        themes: overlays: add acun and bang brightness slider [3/3]
    
        Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>
    
    
    
    
    Change-Id: I5ec57a37727a1d543bc102b84f1d8cfc2d85af3e
    
    Co-authored-by: SamarV-121 <samarvispute121@pm.me>
    Co-authored-by: Ghosuto <clash.raja10@gmail.com>
    Co-authored-by: Pzqqt <821026875@qq.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>

--- config.mk
+++ config.mk
@@ -385,6 +385,21 @@ PRODUCT_PACKAGES += \
     VolumeShadedLayer \
     VolumeAOSPRevamped
 
+# Brightness slider styles
+PRODUCT_PACKAGES += \
+    BrightnessSliderAcunOverlay \
+    BrightnessSliderBangOverlay \
+    BrightnessSliderCyberpunkOverlay \
+    BrightnessSliderFilledOverlay \
+    BrightnessSliderGradientRoundedBarOverlay \
+    BrightnessSliderLeafyOutlineOverlay \
+    BrightnessSliderMinimalThumbOverlay \
+    BrightnessSliderOutlineOverlay \
+    BrightnessSliderRoundedClipOverlay \
+    BrightnessSliderShadedOverlay \
+    BrightnessSliderThinOverlay \
+    BrightnessSliderTranslucentOverlay
+
 # Include {Lato,Rubik} fonts
 $(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
 $(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
